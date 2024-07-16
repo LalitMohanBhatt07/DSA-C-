960d9beb4a7e28e864efd357f61203f33a84d23a
Lb181808 <lb181808@gmail.com>
2024-07-16 13:22:01 +0530

    "Largest Element in Sorted and Rotated Array"

---
+++ _44LargestElementInSortedAndRotatedArray.cc
@@ -0,0 +1,32 @@
+// here pivot element is largest element
+#include<iostream>
+using namespace std;
+
+int findPivotIndex(int arr[],int size){
+    int start=0, end=size-1, mid, ans;
+    if(start==end){
+        return start;
+    }
+    while(start<=end){
+    mid=start+(end-start)/2;
+    if(mid<arr[mid-1]){
+        return mid-1;
+    }
+    else if(arr[mid]>arr[mid+1]){
+        return mid;
+    }
+    else if(arr[start]>arr[mid]){
+        end=mid-1;
+    }
+    else{
+        start=mid+1;
+    }
+}
+return -1;
+}
+
+int main(){
+    int arr[]={12,14,16,2,4,6,8,10};
+    int ans=findPivotIndex(arr,8);
+    cout<<"Largest Element is : "<<arr[ans];
+}
