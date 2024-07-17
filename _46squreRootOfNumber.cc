cac0758b10941f8121e937c9c738cb5b95700cba
Lb181808 <lb181808@gmail.com>
2024-07-17 19:11:57 +0530

    Closest Squre root of any number

---
+++ _46squreRootOfNumber.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int squreRoot(int x){
+    int start=0,end=x,ans=-1;
+    while(start<=end){
+        long long int mid=start+(end-start)/2;
+        if(mid*mid==x){
+            return mid;
+        }
+        else if(mid*mid<x){
+            //go right
+            ans=mid;
+            start=mid+1;
+        }
+        else{
+            //go left
+           
+            end=mid-1;
+        }
+        
+    }
+    return ans;
+}
+
+int main(){
+    int num=68;
+    int result=squreRoot(num);
+    cout<<"Squre root of "<<num<<"is : "<<result;
+}
