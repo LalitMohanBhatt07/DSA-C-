b2cd10aafc904e961017a017bbbb1b4e3818578a
Lb181808 <lb181808@gmail.com>
2024-08-28 18:34:28 +0530

    Insertion sort

---
+++ _53insertionSort.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void print(vector<int> v){
+    for(int i=0;i<v.size();insertionSort++){
+        cout<<v[i]<<" ";
+    }
+}
+
+void insertionSort(vector<int> &v){
+    int n=v.size();
+    for(int i=0;i<n;i++){
+        int key=v[i];
+        int j=i-1;
+        //move elements of arry[0...i-1] that are greater than key
+        while(j>=0 && v[j]>key){
+            v[j+1]=v[j];
+            j--;
+        }
+        v[j+1]=key;
+
+    }
+}
+
+int main(){
+    vector<int>v={5,4,3,2,1};
+
+    insertionSort(v);
+    print(v);
+}
