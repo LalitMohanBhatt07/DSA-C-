f217f419d24049826b996079f9afc82fa8b81d40
Lb181808 <lb181808@gmail.com>
2024-08-31 12:38:48 +0530

    Replace @ with space

---
+++ _103replace@WithSpace.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+void replaceCharacter(char ch[],int n){
+    int index=0;
+    while(ch[index]!='\0'){
+        if(ch[index]=='@'){
+            ch[index]=' ';
+        }
+        index++; 
+    }
+}
+
+int main(){
+    char ch[100];
+    cin.getline(ch,100);
+    int size=strlen(ch);
+    cout<<"String before replacing @ with space is : "<<ch;
+    replaceCharacter(ch,size);
+    cout<<"String after replacing @ with space is : "<<ch;
+    
+}
