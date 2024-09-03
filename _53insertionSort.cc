b281c165b511759d66904d5b5ade78a06af93219
Lb181808 <lb181808@gmail.com>
2024-09-03 23:34:35 +0530

    changes

--- _53insertionSort.cc
+++ _53insertionSort.cc
@@ -13,7 +13,7 @@ void insertionSort(vector<int> &v){
     for(int i=0;i<n;i++){
         int key=v[i];
         int j=i-1;
-        //move elements of arry[0...i-1] that are greater than key
+        //move elements of arry[0...i-1] that are greater than k
         while(j>=0 && v[j]>key){
             v[j+1]=v[j];
             j--;
@@ -27,5 +27,5 @@ int main(){
     vector<int>v={5,4,3,2,1};
 
     insertionSort(v);
-    print(v);
+    print(v)
 }
