a12415e517117f7b796bacf9e2ad35471c6ee69e
Lb181808 <lb181808@gmail.com>
2024-09-09 23:26:19 +0530

    insert at head at doubly linked list

--- _201doublyLinkedList.cc
+++ _201doublyLinkedList.cc
@@ -21,17 +21,44 @@ class Node{
 
 void print(Node* head){
     Node* temp=head;
-    while(head!=NULL){
+    while(temp!=NULL){
         cout<<temp->data<<" ";
         temp=temp->next;
     }
+    cout << endl;
 }
 
-void findLength(Node* head){
+int findLength(Node* head){
     int len=0;
     Node* temp=head;
     while(temp!=NULL){
-        temp=temp->next;
         len++;
+        temp=temp->next;
     }
+    return len;
+}
+
+void insertAtHead(Node* &head, Node* &tail,int data){
+    if(head==NULL){
+        Node* newNode=new Node(data);
+        head=newNode;
+        tail=newNode;
+    }
+    else{
+       Node* newNode=new Node(data);
+       newNode->next=head;
+       head->prev=newNode->prev;
+       head=newNode; 
+    }
+}
+
+int main(){
+    Node* head=NULL;
+    Node* tail=NULL;
+
+    insertAtHead(head,tail,40);
+    insertAtHead(head,tail,30);
+    insertAtHead(head,tail,20);
+    insertAtHead(head,tail,10);
+    print(head);
 }
