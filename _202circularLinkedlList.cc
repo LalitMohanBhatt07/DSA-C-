21efc13b07c8b7adebbce86c648ae54f31eb15f5
Lb181808 <lb181808@gmail.com>
2024-09-12 00:15:16 +0530

    Insert at head at doubly linked list

--- _202circularLinkedlList.cc
+++ _202circularLinkedlList.cc
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Node{
+    public:
     int data;
     Node* next;
 
@@ -15,6 +16,24 @@ class Node{
         this->next=NULL;
     }
 };
+
+void insertAtHead(Node* &head, int data){
+    Node* newNode=new Node(data);
+
+    if(head==NULL){
+        newNode->next=newNode;
+        head=newNode;
+    }
+    else{
+        Node* temp=head;
+        while(temp->next!=head){
+            temp=temp->next;
+        }
+        temp->next=newNode;
+        newNode->next=head;
+        head=newNode;
+    }
+}
 
 int main(){
 
