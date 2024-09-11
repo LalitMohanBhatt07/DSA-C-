974b465ba8661d9993c6b4bbb64c5a0a2a711a56
Lb181808 <lb181808@gmail.com>
2024-09-12 00:05:49 +0530

    Created class for circular linked list

---
+++ _202circularLinkedlList.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    int data;
+    Node* next;
+
+    Node(){
+        this->data=NULL;
+        this->next=NULL;
+    }
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+int main(){
+
+}
