1d3901fb3a1f590a7153c98a8f5a9416c7db6478
Lb181808 <lb181808@gmail.com>
2024-09-20 00:50:38 +0530

    Starting point of loop

--- _208startingPointOfLoop.cc
+++ _208startingPointOfLoop.cc
@@ -1,8 +1,45 @@
 #include<iostream>
-using namespace std
+using namespace std;
 
 class Node{
     public:
     int data;
-    Node *next;
+    Node* next;
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+Node* findStartingPoint(Node* head){
+    Node* slow=head;
+    Node* fast=head;
+
+    while(fast!=NULL){
+        fast=fast->next;
+        if(fast!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+        }
+
+        if(fast==slow){
+            break;
+        }
+    }
+
+    if(fast==NULL){
+        return NULL;
+    }
+
+    slow=head;
+    while(slow!=fast){
+        slow=slow->next;
+        fast=fast->next;
+    }
+
+    return slow;
+}
+
+int main(){
+
 }
