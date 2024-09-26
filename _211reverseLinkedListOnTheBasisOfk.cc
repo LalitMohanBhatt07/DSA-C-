fa77c42ebf15e8d7d0a64a6017a5944001e4271d
Lb181808 <lb181808@gmail.com>
2024-09-26 23:54:27 +0530

    helper function for reverse linked list in k group

--- _211reverseLinkedListOnTheBasisOfk.cc
+++ _211reverseLinkedListOnTheBasisOfk.cc
@@ -1,1 +1,52 @@
 //reverse linked list on the group of k
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+void insertAtHead(Node* &head,int data){
+    if(head==NULL){
+        Node* newNode=new Node(data);
+        head=newNode;
+    }
+    else{
+        Node* newNode=new Node(data);
+        newNode->next=head;
+        head=newNode;
+    }
+}
+
+void printLinkedList(Node* head){
+    Node* temp=head;
+
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    
+}
+
+
+
+int main(){
+    Node* head=NULL;
+    insertAtHead(head,5);
+    insertAtHead(head,4);
+    insertAtHead(head,3);
+    
+
+    printLinkedList(head);
+
+
+}
+
