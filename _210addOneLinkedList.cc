ccd4ac31cf957b26a37470da3f86e48bab2d84fe
Lb181808 <lb181808@gmail.com>
2024-09-26 23:46:30 +0530

    reverse linked list

--- _210addOneLinkedList.cc
+++ _210addOneLinkedList.cc
@@ -12,7 +12,29 @@ class Node{
     }
 };
 
-Node* reverse(Node* head){
+void printLinkedList(Node* head){
+    Node* temp=head;
+
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    
+}
+
+void insertAtHead(Node* &head,int data){
+    if(head==NULL){
+        Node* newNode=new Node(data);
+        head=newNode;
+    }
+    else{
+        Node* newNode=new Node(data);
+        newNode->next=head;
+        head=newNode;
+    }
+}
+
+Node* reverseLinkedList(Node* &head){
     Node* prev=NULL;
     Node* curr=head;
 
@@ -22,25 +44,51 @@ Node* reverse(Node* head){
         prev=curr;
         curr=next;
     }
-
     head=prev;
+    return head;
 }
 
 void addOne(Node* &head){
-    //reverse
-    head=reverse(head);
-     
-    //add
-    int carr=1;
+    head=reverseLinkedList(head);
     Node* temp=head;
+    int carry=1;
 
-    while(head!=NULL){
+    while(temp->next!=NULL){
+        int totalSum=temp->data+carry;
+        int digit=totalSum%10;
+        carry=totalSum/10;
+        temp->data=digit;
 
+        if(carry==0){
+            break;
+        }
+        
     }
 
-    //reverse
+    if(carry!=0){
+        int totalSum=temp->data+carry;
+        int digit=totalSum%10;
+        carry=totalSum/10;
+
+        temp->data=digit;
+        Node* newNode=new Node(carry);
+        temp->next=newNode;
+    }
+
+    reverseLinkedList(head);
 }
 
 int main(){
+    Node* head=NULL;
+    insertAtHead(head,5);
+    insertAtHead(head,4);
+    insertAtHead(head,3);
+    
 
+    printLinkedList(head);
+
+    addOne(head);
+
+    cout<<endl;
+    printLinkedList(head);
 }
