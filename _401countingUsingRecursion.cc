0a0c240f2013add083a8aba55a19a447a8734155
Lb181808 <lb181808@gmail.com>
2024-09-29 23:38:25 +0530

    counting of number using recursion

---
+++ _401countingUsingRecursion.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void printing(int n){
+    if(n==1){
+        cout<<1;
+        return;
+    }
+    cout<<n<<endl;
+    printing(n-1);
+}
+
+int main(){
+    printing(5);
+}
