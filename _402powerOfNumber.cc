1b441343d5b58b4421464e47156cbf9838c1a387
Lb181808 <lb181808@gmail.com>
2024-09-30 23:19:50 +0530

    power of number

---
+++ _402powerOfNumber.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int pow(int num){
+    if(num==0){
+        return 1;
+    }
+    int ans=2*pow(num-1);
+    return ans;
+}
+
+int main(){
+    cout<<pow(10);
+}
