a379a24f4d30254abe0bd81c2e953a9df28253d9
Lb181808 <lb181808@gmail.com>
2024-10-02 23:54:29 +0530

    insert at tail

--- _202circularLinkedlList.cc
+++ _202circularLinkedlList.cc
@@ -34,7 +34,18 @@ void insertAtHead(Node* &head, int data){
         head=newNode;
     }
 }
-
+// void insertAtTail(Node* &head, int data){
+//     Node* newNode=new Node(data);
+//     if(head==NULL){
+//         head=newNode;
+//         return;
+//     }
+//     Node* temp=head;
+//     while(temp->next!=NULL){
+//         temp=temp->next;
+//     }
+//     temp->next=newNode;
+// }
 
 int main(){
     Node* head=NULL;
